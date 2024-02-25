07d13a82ef896df6b1274633fad7fdf5cb043a05
Patrik Americo <patrikamerico5@gmail.com>
2024-02-25 14:24:18 -0300

    Formulario e Google maps

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,18 @@
 from flask import render_template
 from app import app
 
+class Posto:
+    def __init__(self, lat: str, lon: str, id: int) -> None:
+        self.lat = lat
+        self.lon = lon
+        self.id = id
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    cords = [ 
+             Posto("-6.76","-38.96", 1),
+             Posto("-3.67","-38.97", 2),
+             Posto("-5.91","-39.26", 3) 
+            ]
+
+    return render_template("index.html", cords = cords)
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+function OnclickMarker(marker) {
+  const postID = marker.getAttribute('value')
+  document.getElementById('inputId').value = postID;
+}
+
+const markers = document.querySelectorAll("gmp-advanced-marker")
+
+markers.forEach((marker) => {
+  marker.addEventListener('click', () => { OnclickMarker(marker) })
+  
+})
