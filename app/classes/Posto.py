730bb4ecbc9d9b413d9ba7d4b3b2e87544dbeff8
Patrik Americo <patrikamerico5@gmail.com>
2024-02-25 15:08:26 -0300

    Pasta classes

---
+++ app/classes/Posto.py
@@ -0,0 +1,5 @@
+class Posto:
+    def __init__(self, lat: str, lon: str, id: int) -> None:
+        self.lat = lat
+        self.lon = lon
+        self.id = id
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,11 +1,6 @@
 from flask import render_template
 from app import app
-
-class Posto:
-    def __init__(self, lat: str, lon: str, id: int) -> None:
-        self.lat = lat
-        self.lon = lon
-        self.id = id
+from classes import Posto
 
 @app.route("/")
 def index():
